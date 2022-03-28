9c19f4f9fdffe757a8780c24d6d489da3377fb3e
“shrutishende” <shrutishende11@gmail.com>
2022-03-28 20:21:18 +0530

    second commit

---
+++ Anon/assets/js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+//modal variables
+
+const modal = document.querySelector("[data-modal]");
+const modalCloseBtn = document.querySelector("[data-modal-close]");
+const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
+
+// modal function
+
+const modalcloseFun = function () {
+    modal.classList.add("closed");
+};
+
+modalCloseOverlay.addEventListener("click", modalcloseFun);
+modalCloseBtn.addEventListener("click", modalcloseFun);
+
+// notification toast variable
+
+const notificationToast = document.querySelector("[data-toast]");
+const toastCloseBtn = document.querySelector("[data-toast-close]");
+
+toastCloseBtn.addEventListener("click", function () {
+    notificationToast.classList.add("closed");
+});
+
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll(
+    "[data-mobile-menu-open-btn]"
+);
+const mobileMenu = document.querySelectorAll("[data-mobile-menu]");
+const mobileMenuCloseBtn = document.querySelectorAll(
+    "[data-mobile-menu-close-btn]"
+);
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+    //mobile menu function
+    const mobileMenuCloseFunc = function () {
+        mobileMenu[i].classList.remove("active");
+        overlay.classList.remove("active");
+    };
+    mobileMenuOpenBtn[i].addEventListener("click", function () {
+        mobileMenu[i].classList.add("active");
+        overlay.classList.add("active");
+    });
+
+    mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
+    overlay.addEventListener("click", mobileMenuCloseFunc);
+}
+
+// accordion Variabls
+const accordionBtn = document.querySelectorAll("[data-accordion-btn]");
+const accordion = document.querySelectorAll("[accordion-data]");
+
+for (let i = 0; i < accordionBtn.length; i++) {
+    accordionBtn[i].addEventListener("click", function () {
+        const clickedBtn = this.nextElementSibling.classList.contains("active");
+
+        for (let i = 0; i < accordion.length; i++) {
+            if (clickedBtn) break;
+            if (accordion[i].classList.contains("active")) {
+                accordion.classList.remove("active");
+                accordionBtn[i].classList.add("active");
+            }
+        }
+
+        this.nextElementSibling.classList.toggle("active");
+        this.classList.toggle("active");
+    });
+}
